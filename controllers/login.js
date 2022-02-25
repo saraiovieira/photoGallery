5add1d15dba3fe81decbe329ee4218ea02570516
saraiovieira <saravieira09@hotmail.com>
2022-02-25 11:11:51 +0000

    change errors messages and remove comments

--- controllers/login.js
+++ controllers/login.js
@@ -1,33 +1,34 @@
 import { User } from '../model/user.js';
 import bcrypt from 'bcrypt';
 
+
 export const getLogin = (req, res) => {
   res.render("login");
 };
 
+
 export async function login (req, res) {
   
   try {
-    // Get user input
+    
     const { email, password } = req.body;
 
-    // Validate user input
     if (!(email && password)) {
-      return res.status(400).send("All input is required");
+      return res.render("login", {errors: "The email field or password field is empty"});
     }
-    // Validate if user exist in the database
+
     const user = await User.findOne({ email });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      // user
       req.session.isAuth = true;
       res.redirect('/upload');
     }
     else{
-        res.status(400).send("Invalid Credentials");
+      return res.render("login", {errors: "Invalid credentials"});
     }
 
   } catch (err) {
     console.log(err);
   }  
 }
+

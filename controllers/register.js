7d0b17bab66e7a47349bfeba05dd4c0f859b6f8e
saraiovieira <saravieira09@hotmail.com>
2022-02-25 11:12:27 +0000

    add better validation

--- controllers/register.js
+++ controllers/register.js
@@ -1,37 +1,53 @@
 import { User } from '../model/user.js'
 import bcrypt from 'bcrypt'
+import { check, validationResult } from 'express-validator'
+
+export const validateRegister = (method) => {
+    switch (method) {
+      case 'register': {
+       return [ 
+            check('email', 'Email is required')
+                .isEmail(),
+            check('password', 'Password is required')
+            .isLength({ min: 1 })
+        ]   
+      }
+    }
+}
 
 export const register = async (req, res) => {
-
     try {
-        // Get user input
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                success: false,
+                errors: errors.array()
+            });
+        }
+
         const { email, password } = req.body;
-
-        // Validate user input
-        if (!(email && password )) {
-        res.status(400).send("All input is required");
-        }
-
-        // check if user already exist
-        // Validate if user exist in our database
+    
         const oldUser = await User.findOne({ email });
-
+    
         if (oldUser) {
-        return res.status(409).send("User Already Exist. Please Login");
+            return res.status(409).send("User Already Exist. Please Login");
         }
 
-        //Encrypt user password
         const encryptedPassword = await bcrypt.hash(password, 10);
 
-        // Create user in the database
         const user = await User.create({
-        email: email.toLowerCase(), // sanitize: convert email to lowercase
-        password: encryptedPassword,
+            email: email.toLowerCase(), 
+            password: encryptedPassword,
         });
 
-        // return new user
+        res.status(200).json({
+            success: true,
+            message: 'Register was successful',
+        })
+
         await user.save();
-    } catch (err) {
-        console.log(err);
+    } catch(err) {
+        return next(err)
     }
 };
